338dae0bcf11ad9a888cf9bab8b28c17da419ad1
uberflashx <uberflashx@gmail.com>
2025-10-24 23:41:07 +0300

    Playwright installation

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,1 +1,12 @@
-print('Hello!')
+from playwright.sync_api import sync_playwright
+
+
+def test_authorization():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://www.vseinstrumenti.ru/')
+
+        page.wait_for_timeout(100000)
