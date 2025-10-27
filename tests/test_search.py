24ac3036e35856ba9b4bd9baece066f0a3088673
uberflashx <uberflashx@gmail.com>
2025-10-27 17:24:21 +0300

    Adding parametrization for search test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,13 +1,14 @@
 from playwright.sync_api import expect, Page
 import pytest
 
+@pytest.mark.parametrize('product, brand', [('газонокосилка', 'Makita'), ('кусторез', 'Dewalt')])
 @pytest.mark.smoke
 @pytest.mark.search
-def test_search_for_product(chromium_page: Page):
+def test_search_for_product(chromium_page: Page, product: str, brand: str):
     chromium_page.goto('https://www.vseinstrumenti.ru/')
 
     search_input = chromium_page.locator("[data-qa='header-search-input']")
-    search_input.fill('газонокосилка Makita')
+    search_input.fill(f'{product} {brand}')
 
     search_button = chromium_page.locator("[data-qa='header-search-button']")
     search_button.click()
