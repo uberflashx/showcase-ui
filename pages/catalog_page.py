441d6b1896c526639a5c5622d011a2fcde18fc8c
uberflashx <uberflashx@gmail.com>
2025-12-19 00:06:11 +0300

    Adding allure steps

--- pages/catalog_page.py
+++ pages/catalog_page.py
@@ -1,4 +1,5 @@
 from asyncio import wait_for
+import allure
 
 from components.authentication.authorization_modal_window_component import AuthorizationModalWindowComponent
 from pages.base_page import BasePage
@@ -21,29 +22,38 @@ class CatalogPage(BasePage):
         self.user_menu = page.locator("[data-qa='user-menu']")
 
     def fill_search_field(self, search_string: str):
-        self.search_input.fill(search_string)
-        expect(self.search_input).to_have_value(search_string)
+        with allure.step(f'Filling search input with search string: {search_string}'):
+            self.search_input.fill(search_string)
+            expect(self.search_input).to_have_value(search_string)
 
     def click_search_button(self):
-        self.search_button.click()
+        with allure.step('Clicking button "Search"'):
+            self.search_button.click()
 
     def check_visible_results_tiles(self):
-        expect(self.search_results_tiles).to_be_visible()
+        with allure.step('Checking that results tiles are visible'):
+            expect(self.search_results_tiles).to_be_visible()
 
     def click_first_results_tile(self):
-        self.first_results_tile.click()
+        with allure.step('Clicking on the first results tile'):
+            self.first_results_tile.click()
 
     def click_tile_add_to_cart_button(self):
-        self.product_tile_cart_button.click()
+        with allure.step('Clicking button "Add to cart"'):
+            self.product_tile_cart_button.click()
 
     def click_pupup_go_to_cart_button(self):
-        self.pupup_go_to_cart_button.click()
+        with allure.step('Clicking button "Go to cart"'):
+            self.pupup_go_to_cart_button.click()
 
     def check_visible_login_button(self):
-        expect(self.login_button).to_be_visible()
+        with allure.step('Checking that login button is visible'):
+            expect(self.login_button).to_be_visible()
 
     def click_login_button(self):
-        self.login_button.click()
+        with allure.step('Clicking button "Login"'):
+            self.login_button.click()
 
     def check_visible_user_menu(self):
-        expect(self.user_menu).to_be_visible()
+        with allure.step('Checking that username is visible'):
+            expect(self.user_menu).to_be_visible()
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
+import allure
 
 class ProductPage(BasePage):
     def __init__(self, page: Page):
@@ -11,17 +12,22 @@ class ProductPage(BasePage):
         self.brand_image_link = page.locator("a[class='vzu4Gh']")
 
     def check_visible_product_title(self):
-        expect(self.product_title).to_be_visible()
+        with allure.step('Checking that product title is visible'):
+            expect(self.product_title).to_be_visible()
 
     def check_visible_current_price(self):
-        expect(self.current_price).to_be_visible()
+        with allure.step('Checking that current price is visible'):
+            expect(self.current_price).to_be_visible()
 
     def check_visible_add_to_cart_button(self):
-        expect(self.add_to_cart_button).to_be_visible()
-        expect(self.add_to_cart_button).to_have_text('В корзину')
+        with allure.step('Checking that add to cart button is visible'):
+            expect(self.add_to_cart_button).to_be_visible()
+            expect(self.add_to_cart_button).to_have_text('В корзину')
 
     def click_add_to_cart_button(self):
-        self.add_to_cart_button.click()
+        with allure.step('Clicking button "Add to cart"'):
+            self.add_to_cart_button.click()
 
     def click_brand_image_link(self):
-        self.brand_image_link.click()
+        with allure.step('Clicking on the brand image'):
+            self.brand_image_link.click()
