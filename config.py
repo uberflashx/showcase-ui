84b916176e468526ae6ce9067d2b106fe4e68c19
uberflashx <uberflashx@gmail.com>
2025-12-30 16:28:59 +0300

    Adding dotenv file and refactoring fixtures

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from enum import Enum
-
-from pydantic_settings import BaseSettings
-from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath
+from typing import Self
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
 
 class Browser(str, Enum):
     WEBKIT = "webkit"
@@ -9,22 +9,41 @@ class Browser(str, Enum):
     CHROMIUM = "chromium"
 
 
-class TestUser(BaseSettings):
+class TestUser(BaseModel):
     email: EmailStr
     username: str
     password: str
 
 
-class TestData(BaseSettings):
-    image_png_file: FilePath
-
-
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="."
+    )
+
     app_url: HttpUrl
     headless: bool
     browsers: list[Browser]
     test_user: TestUser
-    test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
     browser_state_file: FilePath
+
+    @classmethod
+    def initialize(cls) -> Self:
+        videos_dir = DirectoryPath("./videos")
+        tracing_dir = DirectoryPath("./tracing")
+        browser_state_file = FilePath("browser-state.json")
+
+        videos_dir.mkdir(exist_ok=True)
+        tracing_dir.mkdir(exist_ok=True)
+        browser_state_file.touch(exist_ok=True)
+
+        return Settings(
+            videos_dir=videos_dir,
+            tracing_dir=tracing_dir,
+            browser_state_file=browser_state_file
+        )
+
+settings = Settings.initialize()
