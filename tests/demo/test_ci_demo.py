b133d27f63633ac50a2b52fe7bf626d720940893
uberflashx <uberflashx@gmail.com>
2026-01-21 17:08:14 +0300

    Test trigger for CI

--- tests/demo/test_ci_demo.py
+++ tests/demo/test_ci_demo.py
@@ -19,6 +19,7 @@ class TestCIDemo:
         catalog_page.check_page_have_title()
 
 
+
     @allure.title('Search for existing item')
     @allure.epic(AllureEpic.CONSUMER)
     @allure.feature(AllureFeature.SEARCH)
